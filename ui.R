74152772693a0e71c75590b26577ebc6a88fd2b5
Roklion <yaodongy1018@gmail.com>
2017-01-02 04:48:24 -0500

    Presentation slides

--- ui.R
+++ ui.R
@@ -49,19 +49,32 @@ shinyUI(fluidPage(
             h2("User Manual"),
             tabsetPanel(
                 tabPanel("Introduction",
-                    p("The prediction algorithm is based on training on a large"),
-                    p("English language database. ")
+                    p("This is a light-weighted Shiny App built for presenting the prediction algorithm to predict the next word."),
+                    p("The prediction algorithm is based on training on a large English language database."),
+                    p("The prediction model relies on the assumption that frequency and sequence of the sentences in the language database is a good representation"),
+                    p("of English language."),
+                    p(" "),
+                    
+                    h3("Inputs and Configurations"),
+                    p("Once the loading bar disappears and message 'Prediction Tool is ready!' shows below the 'Input Text String' box, it means the model has been"),
+                    p("loaded and ready for prediction. User can then input the string for predicting the possible next word."),
+                    p("Before start predicting, the user can use the 'Configuration' section to choose wether show probability of each word appearing next, as well as"),
+                    p("total number of results showing."),
+                    p(" "),
+                    
+                    h3("Predictions"),
+                    p("By clicking 'Predict!' below the input box, the prediction algorithm will pick up the string user input and start generating results. Once the"),
+                    p("loading bar on the bottom right corner fully progresses and disappears. The result will show on the top right section of the App page."),
+                    p("If the prediction failed, due to insufficient data in the database or invalid input string, a message will pop up above the output section to"),
+                    p("indicate the failure. Otherwise, the message will read 'Prediction Done!' and prediction will show in form of table. The information in the table"),
+                    p("is based on user's configuration (number of results and whether to show probability).")
                 ),
-    
-                tabPanel("Inputs", p("Describe inputs")),
                 
-                tabPanel("Predictions", p("Describe prediction table")),
-
                 tabPanel("Source Code & Deployment",
                     p("Full source code of this Shiny app can be found on github at"),
-                    p("https://github.com/Roklion/Developing_Data_Products"),
+                    p("https://github.com/Roklion/Data_Capstone"),
                     p("The app is also deployed online at"),
-                    p("https://roklion.shinyapps.io/Developing_Data_Products")
+                    p("https://roklion.shinyapps.io/Data_Capstone")
                 )
             )
         )
